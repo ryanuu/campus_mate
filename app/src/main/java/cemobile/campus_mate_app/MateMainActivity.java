ea2140dbdc8bf16771af3b70af086969de7aa54d
ryanuu <youqingyuan@cemobile.cn>
2016-01-15 19:26:14 +0800

    commit all

---
+++ app/src/main/java/cemobile/campus_mate_app/MateMainActivity.java
@@ -0,0 +1,18 @@
+package cemobile.campus_mate_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MateMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mate_main);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+}
---
+++ app/src/test/java/cemobile/campus_mate_app/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package cemobile.campus_mate_app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
